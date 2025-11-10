c2a5fefb4cab3f917b4bef8c16c49fb37cb2a894
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2025-11-10 22:50:23 +0900

    refactor: MyPage.tsx 파일 이름 변경 및 수정

--- 제이/Week6/Login_Token/src/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ text, onClick }: { text: string; onClick?: () => void }) {
-  return (
-    <button className="login-btn" onClick={onClick}>
-      {text}
-    </button>
-  );
-}
