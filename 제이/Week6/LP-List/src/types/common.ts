f82f87b015adc2ff14bb76b9041602ecec19ba7a
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2025-11-11 18:07:53 +0900

    공통 API 응답 타입 & 페이지네이션 DTO 정의 추가

---
+++ 제이/Week6/LP-List/src/types/common.ts
@@ -0,0 +1,22 @@
+export type CommonResponse<T> = {
+  status: boolean;
+  statusCode: number;
+  message: string;
+  data: T
+};
+
+export type CursorBaseResponse<T> = {
+  status: boolean;
+  statusCode: number;
+  message: string;
+  data: T
+  nextCursor: number;
+  hasNext: boolean;
+};
+
+export type PaginationDto = {
+  cursor?: number;
+  limit?: number;
+  search?: string;
+  order?: string
+}
