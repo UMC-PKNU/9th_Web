73a440c0263d416579d95ad81db6633a26afdbea
jeonggyoungkim <jeonggyoungkim03@gmail.com>
2025-11-11 17:57:25 +0900

    로그인 상태 관리

--- 제이/Week6/LP-List/src/App.tsx
+++ 제이/Week6/LP-List/src/App.tsx
@@ -8,12 +8,9 @@ import HomeLayout from "./layouts/HomeLayout";
 import ProtectedRoute from "./components/ProtectedRoute";
 import MyPage from "./pages/MyPage";
 import GoogleLoginRedirectPage from "./pages/GoogleLoginRedirectPage";
-
-
-// 홈페이지
-// 로그인
-// 회원가입
-// 마이페이지
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AuthProvider } from "./contexts/AuthContext"; // ✅ 추가 필요
 
 const router = createBrowserRouter([
   {
@@ -24,8 +21,8 @@ const router = createBrowserRouter([
       { path: "/", element: <HomePage /> },
       { path: "login", element: <LoginPage /> },
       { path: "signup", element: <SignupPage /> },
-      {path: "/v1/auth/google/login", element: <GoogleLoginRedirectPage/>},
-      { // 인증이 필요한 라우트
+      { path: "v1/auth/google/login", element: <GoogleLoginRedirectPage /> },
+      {
         path: "my",
         element: (
           <ProtectedRoute>
@@ -37,8 +34,18 @@ const router = createBrowserRouter([
   },
 ]);
 
+
+export const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
 }
 
 export default App;
